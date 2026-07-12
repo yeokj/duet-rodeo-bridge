9fcd12b5b71a2c8c0ac4fbfd98771b19300942a9
Kyame Israel <kyame.israel@gmail.com>
2026-07-12 09:04:30 -0400

    feat(relay): implement non-blocking connection handler lifecycle using async iterator

--- scripts/telemetry_consumer.py
+++ scripts/telemetry_consumer.py
@@ -6,14 +6,24 @@ import os
 frame_size = struct.calcsize("@cdddddid")
 connected_clients = set()
 
+async def handle_client_connection(websocket, path):
+    print("Connection in progress...")
+    connected_clients.add(websocket)
+
+    try:
+        async for message in websocket:
+            pass
+    finally:
+        connected_clients.remove(websocket)
+        await websocket.close()
+
 async def read_fifo_and_broadcast():
     fd = os.open('/tmp/telemetry_fifo', os.O_RDONLY | os.O_NONBLOCK)
-
     loop = asyncio.get_event_loop()
     reader = asyncio.StreamReader()
     protocol = asyncio.StreamReaderProtocol(reader)
+
     await loop.connect_read_pipe(lambda: protocol, os.fdopen(fd, 'rb'))
-    # with open('/tmp/telemetry_fifo', 'rb') as fifo:
     while True:
         buffer = await reader.readexactly(frame_size)
         if not buffer:
